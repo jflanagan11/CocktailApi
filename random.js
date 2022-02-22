af19f63655d996b34ed422354f471a143e0c0c8a
JustinFlan <justin.flanagan@student.fairfield.edu>
2022-02-21 19:52:21 -0500

    added seperate page for random cocktails

--- random.js
+++ random.js
@@ -8,24 +8,49 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 getRandomCocktail();
 
 function displayCocktail(data){
+ 
     const cocktailInfo = data.drinks[0];
-    document.body.append(cocktailInfo.strDrink)
-    // console.log(cocktailInfo.strDrink);
-    // console.log(cocktailInfo.strInstructions);
+    const h1 = document.createElement('h1');
+    h1.classList.add('cocktailHeader');
+    h1.innerText = cocktailInfo.strDrink;
+    document.body.append(h1);
+
+    const img = document.createElement('img');
+    img.classList.add('cocktailImg');
+    img.src = cocktailInfo.strDrinkThumb;
+    document.body.append(img);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('cocktailUl')
+    ul.innerText = "Ingredients"
+    document.body.append(ul);
+
     for(let i =1; i < 15; i++){
       if(cocktailInfo[`strIngredient${i}`] === null){
         break;
       }
       else{
         if(cocktailInfo[`strMeasure${i}`] === null){
-          document.body.append(cocktailInfo[`strIngredient${i}`]);
+          let li = document.createElement('li');
+          li.innerText = cocktailInfo[`strIngredient${i}`];
+          ul.append(li);
         }
         else{
-          document.body.append(cocktailInfo[`strMeasure${i}`]);
-          document.body.append(cocktailInfo[`strIngredient${i}`]);
+          let li = document.createElement('li');
+          li.classList.add('cocktailLi');
+          let measure = cocktailInfo[`strMeasure${i}`]
+          let ingredient = cocktailInfo[`strIngredient${i}`]
+          li.innerText = measure + " " + ingredient;
+          ul.append(li);
+          
         }
     }
     }
+    const instructions = document.createElement('p');
+    instructions.classList.add('cocktailInstructions')
+    instructions.innerText = cocktailInfo.strInstructions;
+    document.body.append(instructions);
+
     
   }
    
